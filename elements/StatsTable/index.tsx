a0e5232a82e5a0c302c3d903e97a6d282699df66
Marcos Brasil <strands-grip.0d@icloud.com>
2022-02-15 14:12:36 -0300

    added stats element. need to fix layout on small screen

---
+++ elements/StatsTable/index.tsx
@@ -0,0 +1,30 @@
+type Props = {
+  title: string;
+  data: Array<{ legend: string; value: string }>;
+};
+
+export default function StatsTable({ title, data }: Props) {
+  return (
+    <div className="flex  flex-col w-fit h-[140px]  pt-4">
+      <div className="z-10 font-bold text-left pl-6 w-full h-ful bg-stone-200">
+        {title}
+      </div>
+      <div className="pl-4  overflow-auto">
+        <table className=" w-full   ">
+          <tbody className="">
+            {data.map((item, idx) => {
+              return (
+                <tr key={idx} className="border-b border-gray-400">
+                  <td className=" w-fit border-b border-r border-gray-400  text-right pr-2">
+                    {item.legend}
+                  </td>
+                  <td className="pl-2 text-left">{item.value}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
--- pages/calculator/index.tsx
+++ pages/calculator/index.tsx
@@ -4,61 +4,31 @@ import { useState, useRef } from "react";
 import { useSelector } from "react-redux";
 
 import EuropeanFormFields from "../../components/EuropeanOptionForm";
+import StatsTable from "../../elements/StatsTable";
 
 import styles from "./index.module.css";
 
 export default function Calculator() {
   return (
     <div className="flex flex-col items-center h-full w-full  rounded-lg pt-10 shadow-2xl border border-gray-300  text-gray-700">
-      <h1 className="flex text-2xl h-fit w-full justify-center">
+      <h1 className="flex text-2xl h-fit w-full justify-center italic">
         European Options Calculator
       </h1>
 
+      <div className="pt-4 w-5/6 border-b border-gray-400"></div>
+
       <div className=" w-full h-full p-2 ">
         <EuropeanFormFields />
-        <div className="flex  flex-col w-[220px] h-[140px] pt-4">
-          <div className="z-10 font-bold text-left pl-6 w-full h-ful bg-stone-200">Vanila Greeks</div>
-          <div className="pl-4  overflow-auto">
-            <table className="w-full  border border-gray-400">
-              {/* <caption className=" font-bold text-left pl-2">
-              Vanila Greeks
-            </caption> */}
-
-              <tbody className="border border-gray-400">
-                <tr className="border border-gray-400">
-                  <td className="border border-gray-400 w-[50px] text-right pr-2">
-                    AA
-                  </td>
-                  <td className="pl-2">BB</td>
-                </tr>
-                <tr>
-                  <td>AA</td>
-                </tr>
-                <tr>
-                  <td>AA</td>
-                </tr>
-                <tr>
-                  <td>AA</td>
-                </tr>
-                <tr>
-                  <td>AA</td>
-                </tr>
-                <tr>
-                  <td>AA</td>
-                </tr>
-                <tr>
-                  <td>AA</td>
-                </tr>
-                <tr>
-                  <td>AA</td>
-                </tr>
-                <tr>
-                  <td>AA</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+        <StatsTable
+          title="Vanila Greeks"
+          data={[
+            { legend: "Implied Volatility", value: "20%" },
+            { legend: "deltay", value: "45" },
+            { legend: "gamma", value: "20" },
+            { legend: "theta", value: "4.6" },
+            { legend: "vega", value: "4" },
+          ]}
+        />
       </div>
     </div>
   );
